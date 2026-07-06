5095e8178a12f503428aacab74718450af2fdee3
ashtjaTM <ashtja.prasad@testingmavens.com>
2026-07-06 17:40:14 +0530

    Delete TC1 directory

--- TC1/Validation_Message_field_enforces_max_length_and_shows_clear_error_when_exceeded.ts
+++
@@ -1,5 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test("Validation: Message field enforces max length and shows clear error when exceeded", async ({ page }) => {
-  // No steps generated yet
-});
